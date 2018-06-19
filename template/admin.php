76d0d11dc650464bdaf0df60016f9bea67709671
Ducthman1990 <nirjharmistry@hotmail.com>
2018-06-19 18:31:19 +0530

    URL added.

--- template/admin.php
+++ template/admin.php
@@ -1,3 +1,7 @@
-<?php 
-	echo 'admin';
-?>
+<div class="container">
+    <h2>Admin Panel for CSV Data Upload.</h2>
+    <div class="panel panel-default">
+        <a href="<?php echo home_url('/'); ?>wp-admin/admin.php?page=dividend_upload">Dividend Data Upload</a>
+        <a href="<?php echo home_url('/'); ?>wp-admin/admin.php?page=share_upload">Share Price Upload</a>
+    </div>
+</div>
